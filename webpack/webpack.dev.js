8e5f5dc3e8185900747a0b159ef02c1efc4360ed
sujilnt <sujilnt@gmail.com>
2019-06-16 23:49:51 +0100

    adding webpack files

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,1 +1,24 @@
-console.log("this is a js file");
+const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports=()=>({
+	mode:"development",
+	devtool: "eval-source-map",
+	resolve : {
+		modules: [path.resolve(__dirname,"src"),"node_modules"]
+	},
+	module:{
+		rules : [
+			{
+				test:/\.(js|jsx)$/,
+				exclude : /node_modules/,
+				use:{
+				  loader: "babel-loader"
+				}
+			}
+		]
+	}
+	
+});
+
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,1 +1,9 @@
-console.log("this is a js file");
+const path = require('path');
+module.exports=()=>({
+	mode: "production",
+	output : {
+		filename : "[chunkhash].js"
+	},resolve : {
+		modules: [path.resolve(__dirname,"src"),"node_modules"]
+	},
+});
