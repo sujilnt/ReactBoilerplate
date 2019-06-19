658eec0aeff09eddfd033cc68bfebb6d217495e4
sujilnt <sujilnt@gmail.com>
2019-06-19 13:27:40 +0100

    added couple of new packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 const modeConfig = env => require(`./webpack/webpack.${env}.js`)(env);
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports=({mode,presets}={mode: "development", presets:[]})=>{
 return merge({
 	mode,
@@ -11,12 +12,16 @@ return merge({
 		filename: "./main.js",
 	},
 	plugins: [
+		new webpack.ProgressPlugin(),
+		new CleanWebpackPlugin({
+			dry:true,
+			verbose:true,
+		}),
 		new HtmlWebpackPlugin({
 			template: path.resolve('./webpack/template.html') ,
 			templateParameters:true,
 			inject:true,
 		}),
-		new webpack.ProgressPlugin(),
 	]
 },modeConfig(mode));
 };
