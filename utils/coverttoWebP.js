44672d0ac6e7c5596f7eb176d30551facdcf97f6
sujilnt <sujilnt@gmail.com>
2019-06-20 14:19:39 +0100

    adding utility function to covert images to webp

--- utils/coverttoWebP.js
+++ utils/coverttoWebP.js
@@ -1,1 +1,13 @@
-console.log("this is a js file");
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+const fileName  = [process.argv.pop()] || ['assets/*.{jpg,png}'];
+
+
+imagemin(fileName, 'assets/', {
+	use: [
+		imageminWebp({quality: 50})
+	]
+}).then(() => {
+	console.log('Images optimized');
+});
+
