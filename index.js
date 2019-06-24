61e041a9a7abeb7c1e5c9cbe1cfe217641dfa409
sujilnt <sujilnt@gmail.com>
2019-06-24 11:23:40 +0100

    renaming and imageCOverting Utils

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "./src/main.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App';
+import App from './src/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById("root"));
