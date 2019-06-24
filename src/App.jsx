6f966f68cc68bdffa2d4f8fb9bfe83fe06cb3012
sujilnt <sujilnt@gmail.com>
2019-06-24 13:52:12 +0100

    adding some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import ReactSVGComponent from "./ReactSVGComponent/ReactSVGComponent";
 const App = ()=><div>
 	<div className="container">
 		<h1>This is a heading</h1>
 		<p>This is a paragraph</p>
+		<ReactSVGComponent />
 	</div>
 </div>;
 export default App;
---
+++ src/ReactSVGComponent/ReactSVGComponent.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactSVG from 'react-svg';
+import mess from "../../assets/mess.svg";
+
+const ReactSVGComponet =()=> <div><ReactSVG src={mess} /></div>;
+export default ReactSVGComponet;
