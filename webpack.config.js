817f63d6c89af81e779428e93e1fb39bbcb67294
sujilnt <sujilnt@gmail.com>
2019-07-01 11:56:22 +0100

    adding some more changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const merge = require("webpack-merge");
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path'),
+	webpack = require('webpack'),
+	merge = require("webpack-merge"),
+	HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const modeConfig = env => require(`./webpack/webpack.${env}.js`)(env);
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports=({mode,presets}={mode: "development", presets:[]})=>{
 return merge({
 	mode,
@@ -21,7 +21,7 @@ return merge({
 			template: path.resolve('./webpack/template.html') ,
 			templateParameters:true,
 			inject:true,
-		}),
+		})
 	]
 },modeConfig(mode));
 };
