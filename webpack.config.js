c1252a077e184ee88ed630dcd17a4f93b93ef9ca
sujilnt <sujilnt@gmail.com>
2019-07-08 12:30:50 +0100

    adding some changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path'),
 	HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const modeConfig = env => require(`./webpack/webpack.${env}.js`)(env);
-module.exports=({mode,presets}={mode: "development", presets:[]})=>{
+module.exports=({mode,presets}={mode: "dev", presets:[]})=>{
 return merge({
 	mode,
 	entry:"./index.js",
@@ -18,8 +18,10 @@ return merge({
 			verbose:true,
 		}),
 		new HtmlWebpackPlugin({
-			template: path.resolve('./webpack/template.html') ,
-			templateParameters:true,
+			template: path.resolve('./webpack/template.ejs') ,
+			templateParameters:{
+				title: "React BoilerPlate v3"  // name of the app
+			},
 			inject:true,
 		})
 	]
