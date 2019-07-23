4d0b67f6ee90b0d05359d08253d342f27a9b0430
sujilnt <sujilnt@gmail.com>
2019-07-23 20:07:22 +0100

    adding a demo template

--- src/Components/ReactSVGComponent/ReactSVGComponent.js
+++ src/Components/ReactSVGComponent/ReactSVGComponent.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import ReactSVG from 'react-svg';
+import React,{Component} from 'react';
+import Loadable from 'react-loadable';
 import mess from "assets/mess.svg";
-const image = Object.entries(mess).length === 0 ? "a.svg": mess;
-const ReactSVGComponet =()=> <div><ReactSVG src={image} /></div>;
+
+const L1 = Loadable.Map({
+	loading: () => <div>Loading</div>,
+	loader: {
+		reactsvgCoponent: ()=>import("react-svg"),
+	},
+	render(loaded,props){
+		const ReactSVG = loaded.reactsvgCoponent.default;
+		return <ReactSVG src={props.src} />
+	}
+});
+
+class ReactSVGComponet extends Component{
+	state={
+		loading: false
+	};
+	render(){
+		const image = Object.entries(mess).length === 0 ? "a.svg": mess;
+		if(!this.state.loading){
+			return(
+				<div><L1 src={image}/></div>
+			)
+		}else{
+			return(
+				<div>Loading .....</div>
+			)
+		}
+	}
+	
+}
 export default ReactSVGComponet;
